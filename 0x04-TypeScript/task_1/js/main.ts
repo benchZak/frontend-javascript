4b5afec9a0ba257753676f527f8bcdf222eddf00
benchakar <101592146+benchZak@users.noreply.github.com>
2025-07-09 12:38:11 +0100

    Create main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+// task_1/js/main.ts
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // This allows adding any additional attribute
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
